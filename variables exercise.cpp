10d8271486243dcff12b041d0c7ac5562cd011d8
devinwiggins <theartof97@gmail.com>
2016-09-01 12:15:04 -0500

    variables exercise
    Now that I've learned variables It's time see how much actually stuck.

---
+++ variables exercise.cpp
@@ -0,0 +1,105 @@
+//#1
+// a) 2/3 + 3/5  (0+0=0)
+// b) 2%5 + 3%5  (0+0=0)
+// c) 23%15%-2   (
+// d) 25*1/2     (12)
+// e) 25*1.0/2   (25.0/2=12.5)
+// f) 25*(1/2)   (12)
+// g) 235/8+7    (36)
+// h) 235/8.0+7  (36.375)
+// i) ((20+1)/2-2.0)/(23+3)*0.2  (8.5/5.2=1.63461....)
+
+// #2
+// a) int avgHeight;
+// b) int taxRup;
+// c) int taxRdown;
+// d) int stelibSpent;
+
+// #3
+// a) int 121;
+// b) float 9.4;
+// c) char r;
+// d) int 100000;
+// e) bool False;
+
+// #4
+//#include <iostream>
+//int main()
+//{
+//	int a = 5;
+//	int b = 6;
+//	int c;
+//	c = b;
+//	b = a;
+//	a = c;
+//	std::cout << "a = " << a << std::endl;
+//	std::cout << "b = " << b << std::endl;
+//
+//	system("pause");
+//}
+
+// #5 
+// A)
+	// a) i = 1
+	//    ch = A
+	//    f = 45
+	// b) i = 145
+	//    ch = a
+	//    f = 0
+	// c) i = 1
+	//    ch = a
+	//    f = 45
+	// d) i = 1
+	//    ch = a
+	//    f = 945
+	// e) i = 45
+	//    ch = a
+	//    f = 0
+	// f) i = 15
+	//    ch = a
+	//    f = 0
+// B) 
+//#include <iostream>
+//int main()
+//{
+//	int i = 0;
+//	float f = 0;
+//	char ch = 'a';
+//	std::cin >> i >> ch >> f;
+//	std::cout << "i: " << i << "\nch: " << ch << "\nf: " << f << std::endl;
+//	system("pause");
+	// a) correct 1 A 45
+	// b) wrong... correct answer (i = 1) (ch = A) (f = 45) :1A45
+	// c) wrong... correct answer (i = 1) (ch = 9) (f = 45) :1 9 45
+	// d) wrong... correct answer (i = 1) (ch = 9) (f = 45) :1 945
+	// e) wrong... correct answer (i = 0) (ch = a) (f = 0)  :B 45.6
+	// f) wrong... correct answer (i = 1) (ch = B) (f = 0)  :1BC5.6
+//}
+
+// #6
+//#include <iostream>
+//int main()
+//{
+//	int x;
+//	int y;
+//	int a;
+//	int b;
+//	int c;
+//	float d;
+//	std::cin >> x >> y >> a >> b >> c;
+//	d = (x + y + a + b + c) / 5;
+//	std::cout << "average of these numbers: " << d << std::endl;
+//	system("pause");
+//}
+
+// #7
+//#include <iostream>
+//int main()
+//{
+//	char fLetter = 'a';
+//	char sLetter = 'z';
+//	std::cin >> fLetter >> sLetter;
+// 	char avg = (fLetter + sLetter) / 2;
+//	std::cout << avg;
+//	system("pause");
+//}
