669d5d0eb7f00bc0d814ed3fe2ffaef8cf7e0dd2
devinwiggins <theartof97@gmail.com>
2016-09-02 17:52:00 -0500

    conditionals exercise

---
+++ Conditionals/source.cpp
@@ -0,0 +1,136 @@
+//// #1
+////#include <iostream>
+////int main()
+////{
+////	int y;
+////	int x;
+////	if (y == 0) {
+////		x = 100;
+////	}
+////}
+
+// #2
+//#include <iostream>
+//int main()
+//{
+//	int x;
+//	int y;
+//	std::cout << "please enter a number here. \n";
+//	std::cin >> x;
+//	std::cout << "please enter a bigger or smaller number here. \n";
+//	std::cin >> y;
+//	if (y > x) {
+//		std::cout << y;
+//	}
+//	else {
+//		std::cout << x;
+//	}
+//	system("pause");
+//}
+
+//// #3
+//#include <iostream>
+//int main()
+//{
+//	int x[3]{ 3, 1, 2 };
+//	if (x[3] > 0); {
+//		std::cout << "1, 2, 3 \n";
+//	}
+//	if (x[3] < 0) {
+//		std::cout << "3, 2, 1 \n";
+//	}
+//	else {
+//		std::cout << " 3, 1, 2 \n";
+//	}
+//	system("pause");
+//
+//}
+
+// #4
+//switch (choice)
+//{
+//case 1:
+//	std::cout << "1";
+//	break;
+//case 2 || case 3:
+//		std::cout << "2 or 3";
+//		break;
+//default: 
+//	std::cout << "invalid";
+//	break;
+//}
+
+// #5
+//y = (x == 0) ? y = 0 : y = 10 / x
+
+// #6
+//#include <iostream>
+//int main()
+//{
+//	char myChar;
+//	int a, b;
+//	float answer;
+//	std::cout << "enter an equation using 2 numbers and one operation: (+, -, *, /, %) \n";
+//	std::cin >> a >> myChar >> b;
+//	if (myChar == '-') {
+//		answer = a - b;
+//		std::cout << "=" << answer << std::endl;
+//	}
+//	if (myChar == '+') {
+//		answer = a + b;
+//		std::cout << "=" <<  answer << std::endl;
+//	}
+//	if (myChar == '*') {
+//		answer = a * b;
+//		std::cout << " = " << answer << std::endl;
+//	}
+//	if (myChar == '%') {
+//		answer = a % b;
+//		std::cout << "=" << answer << std::endl;
+//	}
+//	if (myChar == '/') {
+//		answer = a / b;
+//		std::cout << "= " << answer << std::endl;
+//	}
+//	else {
+//		std::cout << "This equation is invalid..." << std::endl;
+//	}
+//	system("pause");
+//}
+
+// #7
+//#include <iostream>
+//int main()
+//{
+//	int jan[31] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25, 26, 27, 28, 29, 30, 31 };
+//	int input;
+//	std::cout << "enter a date in january ranging from 1 to 31 \n";
+//	std::cin >> input;
+//	if (input < 1) {
+//		std::cout << "Error: That is an invalid date. \n";
+//	}
+//	if (input > 31) {
+//		std::cout << "Error: That is an invalid date \n";
+//	}
+//	system("pause");
+//}
+
+// #8
+// a) true
+// b) false
+// c) true
+// d) true
+// e) true
+// f) false
+// g) true
+// h) true
+// i) true
+// j) true
+
+
+// #9
+// a) true
+// b) true
+// c) false
+// d) true
+// e) false
